af3d83270b3924703791c820dc5ed6a9894214fc
nicofo <nicofortezaocana@gmail.com>
2016-03-07 23:11:01 +0100

    Grid Class: putShip, toString, removeShip, nextPosition

--- swd-client/src/model/Grid.java
+++ swd-client/src/model/Grid.java
@@ -16,7 +16,22 @@ public class Grid {
         this.cells = new HashMap<>();
         this.numShipsLeft = 0;
     }
-
+    public String toString (){
+        String mensaje;
+        mensaje=" |0123456789|\n";
+        for(char i='A'; i<='J'; i= (char)(i+1)){
+            mensaje= mensaje+i+"|";
+            for(char j='0'; j<='9'; j= (char)(j+1)){
+                if(cells.containsKey(i+""+j)){
+                    mensaje=mensaje+"X";
+                }else{
+                    mensaje=mensaje + " ";
+                }
+            }
+            mensaje=mensaje+"|\n";
+        }
+        return mensaje;
+    }
     public Message hitCell(String position) {
         return cells.get(position).hitCell(); // TODO switch of cases to return
     }
@@ -26,10 +41,11 @@ public class Grid {
         Ship ship = new Ship(shipSize);
         Cell cell;
         String newPosition;
-        position = position.toUpperCase();
+
         if (position.length() != 2) {
             return false;
         }
+        position = position.toUpperCase();
         character = position.charAt(0);
         number = position.charAt(1);
         if (orientation == Orientation.H) {
@@ -40,7 +56,9 @@ public class Grid {
                 if (invalidCell(character, (char) i)) return false;
             }
             for (int i = number; i <= (number + shipSize - 1); i++) {
-                cell = new Cell(character + "" + ((char) (i)), ship);//TODO Acabar
+                newPosition=(character + "" + ((char) (i)));
+                cell = new Cell(newPosition, ship);
+                cells.put(newPosition, cell);
 
 
             }
@@ -50,23 +68,77 @@ public class Grid {
                 return false;
             }
             for (int i = character; i <= (character + shipSize - 1); i++) {
-                if (invalidCell((char) i, number)) return false;//TODO Acabar
+                if (invalidCell((char) i, number)) return false;
+            }
+            for (int i = character; i <= (character + shipSize - 1); i++) {
+                newPosition=(((char) (i)) + "" + number);
+                cell = new Cell(newPosition, ship);
+                cells.put(newPosition, cell);
+
+
             }
 
         }
-        return true; // TODO check if ship can be put or not
+        return true;
     }
 
 
     public void removeShip(int shipSize, String position, Orientation orientation) {
-        //TODO fill in with code
+        char character, number;
+        String newPosition;
+        position = position.toUpperCase();
+        character = position.charAt(0);
+        number = position.charAt(1);
+        if (orientation == Orientation.H) {
+            for (int i = number; i <= (number + shipSize - 1); i++) {
+                newPosition=(character + "" + ((char) (i)));
+                cells.remove(newPosition);
+            }
+        }else{
+            for (int i = character; i <= (character + shipSize - 1); i++) {
+                newPosition=(((char) (i)) + "" + number);
+                cells.remove(newPosition);
+            }
+        }
     }
 
     public String nextPosition(String position){
+        char character, number;
+        if (position.length() != 2) {
+            return null;
+        }
+        position = position.toUpperCase();
+        character = position.charAt(0);
+        number = position.charAt(1);
+        if(number== '9') {
+            character = ((char) (character + 1));
+            number='0';
 
+        }else{
+            number = ((char) (number + 1));
+        }
+        while(character<= 'J'){
+
+
+            if(!invalidCell(character, number)) return (character + "" +number);
+            if(number== '9') {
+                character = ((char) (character + 1));
+                number='0';
+
+            }else{
+                number = ((char) (number + 1));
+            }
+        }
+        return null;
     }
 
-    public boolean invalidCell(char character, char number) {
+    /**
+     *
+     * @param character
+     * @param number
+     * @return true if is invalid, false if it is valid
+     */
+    private boolean invalidCell(char character, char number) {
 
         return (cells.containsKey(character + number)
                 || cells.containsKey(((char) (character - 1)) + number)
