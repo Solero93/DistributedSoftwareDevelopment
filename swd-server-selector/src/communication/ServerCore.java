dd8f2838d784805898a26e9482a138f546109e74
Solero93 <solero93@gmail.com>
2016-03-26 20:45:19 +0100

    Doing this marvellous Selector

--- swd-server-selector/src/communication/ServerCore.java
+++ swd-server-selector/src/communication/ServerCore.java
@@ -32,7 +32,6 @@ public class ServerCore {
     private CharsetEncoder encoder;
     private CharsetDecoder decoder;
     private ByteBuffer buffer;
-    private HashMap<SelectionKey, Game> games;
     private int numGame;
 
     public ServerCore(int port, String layout, int mode) throws IOException {
@@ -47,7 +46,6 @@ public class ServerCore {
         this.encoder = charset.newEncoder();
         this.decoder = charset.newDecoder();
         this.buffer = ByteBuffer.allocate(512);
-        this.games = new HashMap<>();
         this.numGame = 0;
     }
 
@@ -69,23 +67,27 @@ public class ServerCore {
                             SocketChannel client = this.server.accept();
                             client.configureBlocking(false);
                             SelectionKey clientKey = client.register(this.selector, SelectionKey.OP_READ);
-                            this.games.put(clientKey, new Game(this.layout, this.mode, this.numGame++));
+                            key.attach(new Game(this.layout, this.mode, this.numGame++));
                             System.out.println("Client with address "
                                     + client.socket().getInetAddress() + " connected to server");
                         }
                     } else {
                         SocketChannel client = (SocketChannel) key.channel();
-                        if (!key.isReadable() || client.read(this.buffer) == -1) {
-                            key.cancel();
-                            client.close();
+                        if (!key.isReadable()) {
+                            if (client.read(this.buffer) == -1){
+                                key.cancel();
+                                client.close();
+                            }
                             continue;
                         }
+
                         this.buffer.flip();
                         String request = this.decoder.decode(this.buffer).toString();
+                        this.buffer.clear();
                         System.out.println("Client with address "
                                 + client.socket().getInetAddress() + " sent a message to server: "
                                 + request);
-                        Game clientGame = this.games.get(key);
+                        Game clientGame = (Game)key.attachment();
                         ArrayList<Message> clientMessages = this.readRequest(request);
                         try {
                             for (Message clientMsg : clientMessages) {
@@ -97,11 +99,10 @@ public class ServerCore {
                                 }
                             }
                         } catch (EndGameException ex){
+                            clientGame.close();
                             key.cancel();
                             client.close();
-                            this.games.remove(key);
                         }
-                        this.buffer.clear();
                     }
                 }
             } catch (IOException e) {
@@ -119,6 +120,7 @@ public class ServerCore {
         try {
             for (SelectionKey key : this.selector.keys()){
                 key.channel().close();
+                key.cancel();
             }
             this.server.close();
             this.selector.close();
@@ -128,20 +130,21 @@ public class ServerCore {
 
     }
 
-    private ArrayList<Message> readRequest(String req){
+    private ArrayList<Message> readRequest(String req) throws StringIndexOutOfBoundsException{
         ArrayList<Message> finalMessages = new ArrayList<>(2);
-        for (int i=0; i<req.length() && finalMessages.size() < 2; i+=2){
+        for (int i=0; i<req.length() && finalMessages.size() < 2;){
             String commandCode = req.substring(i,i+4);
             Command cmd = Command.getCommandFromCode(commandCode);
             i+=4;
-            i++;
             String params = null;
             switch(cmd){
                 case FIRE:
+                    i++;
                     params = req.substring(i,i+2);
                     i+=2;
                     break;
                 case ERROR:
+                    i++;
                     int numChars = Integer.parseInt("" + req.charAt(i) + req.charAt(i+1));
                     params = req.substring(i,i+numChars);
                     i+=numChars;
