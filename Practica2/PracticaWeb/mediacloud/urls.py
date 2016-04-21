9065c07b95457eca905be813f1a917d8dea46bc2
nicofo <nicofortezaocana@gmail.com>
2016-04-21 18:43:24 +0200

    Worksgit add -A tuto 1

--- Practica2/PracticaWeb/mediacloud/urls.py
+++ Practica2/PracticaWeb/mediacloud/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^catalog/()$', views.catalog, name='catalog'),
+    url(r'^catalog/(?P<type>.*)/$', views.catalog, name='catalog')
 ]
 
--- Practica2/PracticaWeb/mediacloud/views.py
+++ Practica2/PracticaWeb/mediacloud/views.py
@@ -1,6 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from mediacloud.models import Item
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the mediacloud index.")
+
+
+def catalog(request, type):
+    catalog_by_type = Item.objects.filter(type=type)
+    context = {
+        'catalog': catalog_by_type,
+        'type': type,
+    }
+    return render(request, 'catalog.html', context)
