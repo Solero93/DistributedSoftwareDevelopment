bddde80598da054b8c75b0866418f02cc2a877d0
CHRISTIAN JOSE SOLER <chsolers7.alumnes@ub052229.mat.ub.edu>
2016-05-19 18:08:12 +0200

    Began restful

--- Practica2/PracticaWeb/PracticaWeb/settings.py
+++ Practica2/PracticaWeb/PracticaWeb/settings.py
@@ -36,7 +36,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+      # Use Django's standard `django.contrib.auth` permissions,
+      # or allow read-only access for unauthenticated users.
+      'DEFAULT_PERMISSION_CLASSES': [
+          'rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+      'PAGE_SIZE': 20
+}
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
